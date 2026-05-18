22d1b074e935903a0e263a6db06aaa3a3b1f1b7d
Odusola Adewale Iseoluwa <Iseoluwaodu85@gmail.com>
2026-05-18 13:27:22 -0700

    I implement mobile version

--- src/components/WeBuild.tsx
+++ src/components/WeBuild.tsx
@@ -3,14 +3,15 @@ import React from 'react'
 
 function WeBuild() {
   return (
-    <section className='Inner-wrapper flex items-center justify-center'>
-<div className=' w-[893px] space-y-10 flex flex-col items-center justify-center'> 
-  <Image src="/myVamsnetLogo_white.svg" alt="We Build Image" width={62} height={74}  />
- <div className='flex flex-col items-center justify-center '>
-  <h2 className='text-2xl font-bold text-[#FFFFFF] font-normal text-[58px] leading-20 font-heading'>We Build, Guide, and Empower</h2>
-  <p className='w-[587px] items-center justify-center font-normal text-[20px] leading-9 text-[#FFFFFFCC]'>We build solutions that empower businesses, support individuals, and contribute to economic growth.</p>
+    <section className='Inner-wrapper flex lg:flex-row flex-col items-center justify-center'>
+<div className=' lg:w-[893px] w-[327px] lg:space-y-10  flex flex-col items-center justify-center'> 
+  <Image src="/myVamsnetLogo_white.svg" alt="We Build Image" width={62} height={74}  className='lg:block hidden'/>
+  <Image src="/myVamsnetLogo_white.svg" alt="We Build Image" width={37} height={44}  className='lg:hidden'/>
+ <div className='lg:w-[893px] w-[279px] flex flex-col items-center justify-center space-y-4 '>
+  <h2 className=' lg:text-[58px] text-[32px] lg:font-bold font-normal text-center text-[#FFFFFF] lg:leading-20 leading-12.5 font-heading'>We Build, Guide, and Empower</h2>
+  <p className='lg:w-[587px] w-[279px] text-center font-normal lg:text-[20px] text-[14px] lg:leading-9  text-[#FFFFFFCC]'>We build solutions that empower businesses, support individuals, and contribute to economic growth.</p>
   </div>  
-        <div className="flex gap-5 items-center justify-center">
+        <div className="flex lg:flex-row flex-col gap-5 items-center justify-center">
           <button className="w-73 h-14.75 rounded-lg text-[16px] font-normal leading-[100%] text-brand-lime bg-[#007FFF] hover:bg-[#007FFF] transition-colors duration-300">
             Partner with us
           </button>
