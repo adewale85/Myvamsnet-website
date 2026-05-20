bc2a60a27e78a3d1321dc0f89ae9f45851bcf3b3
Odusola Adewale Iseoluwa <Iseoluwaodu85@gmail.com>
2026-05-20 07:06:45 -0700

    I added and re-constructed ReadyToBuild section also add Lastimage section to the bottom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ import HeroeSection from '@/components/HeroeSection'
 import HowWeWork from '@/components/HowWeWork'
 import Navbar from '@/components/layout/Navbar'
 import OurRecentProject from '@/components/OurRecentProject'
+import ReadyToBuild from '@/components/ReadyToBuild'
 import RecommendedForYou from '@/components/RecommendedForYou'
 import WeBuild from '@/components/WeBuild'
 import WhatWeDoSection from '@/components/WhatWeDoSection'
+import Image from 'next/image'
 
 function page() {
   return (
@@ -34,9 +36,13 @@ function page() {
       <div>
         <RecommendedForYou />
       </div>
-      <div className='relative min-h-[495px] bg-[radial-gradient(circle_at_center,#151B2F_0%,_#004C99_100%,_#004C99_100%)] bg-cover bg-center bg-no-repeat text-center flex items-center justify-center'>
-        <WeBuild />
-      </div>
+      <div className='relative min-h-[495px] bg-[radial-gradient(circle_at_center,#151B2F_0%,_#004C99_100%,_#004C99_100%)] bg-cover bg-center bg-no-repeat text-center flex flex-col text-center items-center justify-center'>
+        <ReadyToBuild />
+        </div>
+      <div>
+     <Image src="/Lastimage.svg" alt="We Build Image" width={1940} height={514} className='lg:block hidden'/>
+   </div>
+      
     </main>
   )
 }
--- src/components/WeBuild.tsx
+++ src/components/WeBuild.tsx
@@ -1,24 +1,32 @@
+import Image from "next/image"
 
 
 function WeBuild() {
   return (
-    <section className='Inner-wrapper flex lg:flex-row flex-col items-center justify-center'>
+    <section className=''>
+<div className="Inner-wrapper flex lg:flex-row flex-col items-center justify-center">
 <div className=' lg:w-[893px] w-[327px] lg:space-y-10  flex flex-col items-center justify-center'> 
-
+  <Image src="/myVamsnetLogo_white.svg" alt="We Build Image" width={62} height={74}  className='lg:block hidden'/>
+      <Image src="/myVamsnetLogo_white.svg" alt="We Build Image" width={37} height={44}  className='lg:hidden'/>
  <div className='lg:w-[918px] w-[279px] flex flex-col items-center justify-center space-y-6 '>
-  <h2 className=' lg:text-[58px] text-[32px] lg:font-bold font-normal text-center text-[#FFFFFF] lg:leading-20 leading-12.5 font-heading'>Ready to Build, Grow, or Scale?</h2>
-  <p className='lg:w-[587px] w-[279px] text-center font-normal lg:text-[20px] text-[14px] lg:leading-9  text-[#FFFFFFCC]'>We help you move forward with clarity and confidence.</p>
+  <h2 className=' lg:text-[58px] text-[32px] lg:font-bold font-normal text-center text-[#FFFFFF] lg:leading-20 leading-12.5 font-heading'>We Build, Guide, and Empower</h2>
+  <p className='lg:w-[587px] w-[279px] text-center font-normal lg:text-[20px] text-[14px] lg:leading-9  text-[#FFFFFFCC]'>We build solutions that empower businesses, support individuals, and contribute to economic growth.</p>
   </div>  
         <div className="flex lg:flex-row flex-col gap-5 items-center justify-center">
           <button className="w-[292px] h-14.75 rounded-lg text-[16px] font-heading font-normal leading-[100%] text-brand-lime bg-[#007FFF] hover:bg-[#007FFF] transition-colors duration-300">
-           Book a Consultation
+           Partner with us
           </button>
           <button className="w-[292px] h-14.75 rounded-lg text-[16px] font-heading font-normal leading-[100%] text-brand-lime border border-brand-lime border-opacity-50 hover:bg-brand-red-500 transition-colors duration-300">
-           Get in Touch
+           Join our Team
           </button>
         </div>
 </div>
-
+    </div>
+    
+      {/* <div>
+        <Image src="/Lastimage.svg" alt="We Build Image" width={1940} height={514} className='lg:block hidden'/>
+      </div> */}
+    
     </section>
   )
 }
